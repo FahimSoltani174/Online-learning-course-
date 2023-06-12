ac7f3f10ae1dd107b759502e934ad1ed5a4096eb
FahimSoltani174 <fahimsoltani194@gmail.com>
2023-06-12 21:16:38 +0430

    Complete the final Decoration of second page

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -9,7 +9,31 @@ class HomePage extends StatelessWidget{
     "Live Course",
     "LeaderBoard",
   ];
-  List<Color>=[]
+  List<Color> catColors= [
+    Color(0xFFFFCF2F),
+    Color(0xFF6FE08D),
+    Color(0xFF61BDFD),
+    Color(0xFFFC7C7F),
+    Color(0xFFCD84FD),
+    Color(0xFF78E667),
+
+  ];
+
+  List<Icon> catIcons = [
+    Icon(Icons.category , color: Colors.white, size: 30),
+    Icon(Icons.video_library , color: Colors.white, size: 30),
+    Icon(Icons.assignment , color: Colors.white, size: 30),
+    Icon(Icons.store , color: Colors.white, size: 30),
+    Icon(Icons.play_circle_fill , color: Colors.white, size: 30),
+    Icon(Icons.emoji_events , color: Colors.white, size: 30),
+  ];
+
+  List imgList = [
+    "flutter.jpg",
+    "react.png",
+    "js.png",
+    "java.png",
+  ];
   @override
   Widget build(BuildContext context){
     return Scaffold(
@@ -77,6 +101,7 @@ class HomePage extends StatelessWidget{
               padding: EdgeInsets.only(top: 20 , left: 15 , right: 15 , ),
             child: Column(children: [
               GridView.builder(
+                itemCount: catNames.length,
                 shrinkWrap: true,
                 physics: NeverScrollableScrollPhysics(),
                 gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
@@ -84,11 +109,111 @@ class HomePage extends StatelessWidget{
                   childAspectRatio: 1.1
                 ),
                 itemBuilder: (context , index){
-                  return Column();
+                  return Column(
+                    children: [
+                      Container(
+                        height: 60,
+                        width: 60,
+                        decoration: BoxDecoration(
+                          color: catColors[index],
+                          shape: BoxShape.circle,
+                        ),
+                        child: Center(child: catIcons[index],),
+                      ),
+                      SizedBox(height: 10),
+                      Text(catNames[index],
+                        style: TextStyle(
+                          fontSize: 16 , fontWeight: FontWeight.w500,
+                          color: Colors.black.withOpacity(0.7),
+                        ),
+                      ),
+                    ],
+                  );
                 },
               ),
-            ],),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("Course" , style: TextStyle (
+                    fontSize: 23 ,
+                    fontWeight: FontWeight.w600
+                  ),
+                  ),
+                  Text("See All" , style: TextStyle (
+                      fontSize: 18 ,
+                      fontWeight: FontWeight.w500,
+                      color: Color(0xFF674AEF),
+                  ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 10 ),
+              GridView.builder(
+                itemCount: imgList.length,
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  childAspectRatio: (MediaQuery.of(context).size.height - 50 - 25) / (4 * 240),
+                  mainAxisSpacing: 10,
+                  crossAxisSpacing: 10,
+                ),
+                itemBuilder: (context , index){
+                  return InkWell(
+                    onTap: (){},
+                    child: Container(
+                      padding: EdgeInsets.symmetric(vertical: 8.0 , horizontal: 10),
+                      decoration:  BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Color(0xFFF5F3FF)
+                      ),
+                      child: Column(children: [
+                        Padding(
+                            padding: EdgeInsets.all(10),
+                            child: Image.asset(
+                              "images/${imgList[index]}",
+                              width: 100,
+                              height: 100,
+                            ),
+                        ),
+                        SizedBox(height: 10,),
+                        Text(
+                          imgList[index],
+                          style: TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.w600,
+                            color: Colors.black.withOpacity(0.6),
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        Text("55 vedios", style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.black.withOpacity(0.5)
+                        ),)
+                      ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ],
+            ),
           )
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        showUnselectedLabels: true,
+        iconSize: 32,
+        selectedItemColor: Color(0xFF674AEF),
+        selectedFontSize: 18,
+        unselectedItemColor: Colors.grey,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home),label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.assignment),label: 'Course'),
+          BottomNavigationBarItem(icon: Icon(Icons.favorite),label: 'Whishlist'),
+          BottomNavigationBarItem(icon: Icon(Icons.person),label: 'Account'),
+
         ],
       ),
     );
